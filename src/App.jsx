d9d18469886125d0c45f98cb27dcb65fd5c080af
clothe09281 <clothe09281@gmail.com>
2026-02-28 16:13:43 +0800

    更新：新增首頁

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-<Routes>
-  <Route path="/" element={<HomePage />} />
-  <Route path="/login" element={<LoginPage />} />
-  <Route path="/student-login" element={<StudentLoginPage />} />
-  <Route path="/student" element={<StudentPage />} />
-  <Route path="/dashboard" element={<DashboardPage />} />
-</Routes>
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import StudentLoginPage from "./pages/StudentLoginPage";
+import StudentPage from "./pages/StudentPage";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
-
-      <Route path="/student-login" element={<StudentLoginPage />} />
-      <Route path="/student" element={<StudentPage />} />
-
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/student-login" element={<StudentLoginPage />} />
+        <Route path="/student" element={<StudentPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
